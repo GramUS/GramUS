b9e2baa65c58061e4e47a164ce3efe8826a9d1f2
Roj Serbest <rojserbest@icloud.com>
2021-07-22 15:13:03 +0300

    Use Api.Ping for pinging

--- src/handlers/ping.ts
+++ src/handlers/ping.ts
@@ -1,9 +1,15 @@
+import { Api } from "telegram";
 import { CommandHandler, getResultText } from "../userbot";
+import bigInt from "big-integer";
 
 export default new CommandHandler(
-    async (_, event) => {
+    async (client, event) => {
         const toSubtract = new Date().getTime();
-        await event.message.edit({ text: "<code>...</code>" });
+        await client.invoke(
+            new Api.Ping({
+                pingId: bigInt(client.session.dcId),
+            }),
+        );
         await event.message.edit({
             text: getResultText(`${new Date().getTime() - toSubtract}ms`),
         });
